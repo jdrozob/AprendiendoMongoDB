c108a017908b401540bceac8acc369304d1eeff5
jdrozob <jdrozob@unal.edu.co>
2013-07-13 18:10:25 -0500

    Se implemementa el blog en el arhivo Blog CRUD.php y se crea el archivo style.css

--- Blog CRUD.php
+++ Blog CRUD.php
@@ -1,7 +1,72 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * En este archivo se creara un blog el cual implementara una conexion con MongoDB y se realizaran operaciones CRUD
  */
+$action = (!empty($_POST['btn_submit']) && ($_POST['btn_submit'] === 'Salvar')) ? 'save_article' : 'show_form';
+switch ($action) {
+    case 'save_article':
+        try {
+            $connection = new Mongo();
+            $database = $connection->selectDB('miBlog');
+            $collection = $database->selectCollection('articles');
+            /* Código alternativo de selección base de datos colección:
+             * $connection = new Mongo();
+             * $collection = $conection->miBlog->articles;
+             */
+            $article = array();
+            $article['title'] = $_POST['title'];
+            $article['content'] = $_POST['content'];
+            $article['saved_at'] = new MongoDate();
+            $collection->insert($article);
+        } catch (MongoConnectionException $e) {
+            die("No se ha podido conectar a la base de datos " . $e->getMessage());
+        } catch (MongoException $e) {
+            die("No se han podido insertar los datos " . $e->getMessage());
+        }
+        /* Código alternativo si queremos que el método insert espere respuesta de MongoDB:
+         * try {
+         * $status = $connection->insert(array('title' => 'Titulo Blog', 'content' => 'Contenido BLog'), array('safe' => True));
+         * echo "Operación de inserción completada";
+         * } catch (MongoCursorException $e) {
+         * die("Insert ha fallado " . $e->getMessage());
+         * }
+         */
+        break;
+    case 'show form':
+    default :
+}
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transtional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <link rel="stylesheet" href="style.css"/>
+        <title>Mi Blog</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <div id="contentarea">
+            <div id="innercontentarea">
+                <h1>Mi Blog</h1>
+                <?php if ($action === 'show_form'): ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <h3>T&Iacute;culo</h3>
+                    <p>
+                        <input type="text" name="title" id="title"/>
+                    </p>
+                    <h3>Contenido</h3>
+                    <textarea name="content" rows="20"></textarea>
+                    <p>
+                        <input type="submit" name="btn_submit" value="Salvar"/>
+                    </p>
+                </form>
+                <?php else: ?>
+                <p>
+                    Art&Aacute;culo salvado. _id:<?php echo $article['_id']; ?>
+                    <a href="Blog CRUD.php"> &iquest;Escribir otro?</a>
+                </p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </body>
+</html>
+
