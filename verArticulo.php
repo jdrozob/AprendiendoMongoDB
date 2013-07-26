520abcefc31172c6c83f41e2d608fdbed4ac1d72
jdrozob <jdrozob@unal.edu.co>
2013-07-25 22:20:52 -0500

    Se implementa la pagina en donde se veran los post completos.

--- verArticulo.php
+++ verArticulo.php
@@ -9,3 +9,20 @@ try {
 }
 $article = $collection->findOne(array('_id' => new MongoId($id)));
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transtional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+    <head>
+        <link rel="stylesheet" href="style.css"/>
+        <title>Blog</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <div id="contentarea">
+            <div id="innercontentarea">
+                <h1>Mi post completo</h1>
+                <h2><?php echo $article['tittle']; ?></h2>
+                <p><?php echo $article['content']; ?></p>
+            </div>
+        </div>
+    </body>
+</html>
